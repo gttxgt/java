90c376baab9ee6133e9ab6af1b08ac2c77490de8
gttxgt <66577023+gttxgt@users.noreply.github.com>
2020-06-07 21:10:05 -0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, T World" in the terminal window.
-      System.out.println("Hello, World");
+      System.out.println("Hello, T World");
    }
 }
